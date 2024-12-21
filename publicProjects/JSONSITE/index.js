7c2bf347333f47d7709ba2ec0080f68d0a91d234
mrkrca <aleksa.djuric.dj@gmail.com>
2024-12-21 17:08:52 +0100

    initial commit

---
+++ publicProjects/JSONSITE/index.js
@@ -0,0 +1,149 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import axios from "axios";
+import fs from "fs"
+import { error } from "console";
+
+
+const app = express();
+const port = 3000;
+
+// ignore database connection
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "permalist",
+    password: "1212",
+    port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var currentDate = new Date().getFullYear()
+
+
+app.get("/", (req, res) => {
+    const query = req.query;
+
+    fs.readFile('movies.JSON', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file:", err);
+            return res.status(500).send("Server Error");
+        }
+
+        const moviesData = JSON.parse(data);
+
+       
+
+       
+        res.render("index.ejs", { movies: moviesData , query: query, currentDate});
+    });
+});
+app.get("/new", (req, res)=>  {
+    res.render("new.ejs", {movies:movies})
+})
+
+app.get("/view/:imdbID", (req, res) => {
+    fs.readFile('movies.JSON', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file:", err);
+            return res.status(500).send("Server Error");
+        }
+
+        const moviesData = JSON.parse(data);
+      
+
+       
+        const movie = moviesData.find((m) => m.imdbID === req.params.imdbID);
+        
+        if (movie) {
+            res.render("view.ejs", { movie , currentDate}); 
+        } else {
+            res.status(404).send('Movie not found');  
+        }
+    });
+});
+
+// not in works
+app.post("/new", (req, res)=>  {
+    var movieName = req.body.movieName;
+    var genre = req.body.genre;
+    var description = req.body.description;
+
+    movies.push({
+        id: movies.length + 1,
+        movieName: movieName,
+        genre: genre,
+        description: description,
+    });
+
+    
+
+
+    res.render("new.ejs", {movies:movies, currentDate})
+    
+})
+
+
+app.post("/filter/genre/:genre", (req, res) => {
+    let genre = req.body.genre;
+  
+    fs.readFile('movies.JSON', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file:", err);
+            return res.status(500).send("Server Error");
+        }
+
+        const moviesData = JSON.parse(data);
+
+        
+        const filteredMovies = moviesData.filter((movie) => {
+            return movie.Genre.includes(genre);
+        });
+        
+    
+        
+        if (filteredMovies.length > 0) {
+            
+            res.render("index.ejs", { movies: filteredMovies, query: req.query, currentDate });
+        } else {
+            res.render("index.ejs", { movies: filteredMovies, query: req.query, currentDate });
+        }
+    });
+});
+
+
+app.post("/search", (req, res) => {
+    const searchQuery = req.body.searchQuery;
+    fs.readFile("movies.JSON", "utf8", (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file:", err);
+            return res.render("index.ejs", { 
+                movies: [], 
+                query: req.query, 
+                currentDate, 
+                errorMsg: "Error loading movies for search." 
+            });
+        }
+        const moviesData = JSON.parse(data);
+        const filteredMovies = moviesData.filter(movie => 
+            movie.Title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        res.render("index.ejs", { 
+            movies: filteredMovies, 
+            query: req.query, 
+            currentDate, 
+            errorMsg: filteredMovies.length === 0 ? "No movies matched your search." : null 
+        });
+    });
+});
+
+
+app.listen(port, ()=> {
+console.log(`Listening to port ${port}`);
+
+})
